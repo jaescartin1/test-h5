3a445c6c6ce43a3a036442d6acb79bd1e5a8baaf
Jose A. Escartin <ja.escartin@gmail.com>
2018-06-04 18:22:56 +0200

    Corrected file

--- print_h5/print_h5.py
+++ print_h5/print_h5.py
@@ -2,21 +2,22 @@
 """Print the contents of an HDF5file"""
 
 import numpy as np
-# ddd
-#np.warnings.filterwarnings("")
+# h5py with numpy >= 1.14 printsan error message on importing, supress it
+np.warnings.filterwarnings('ignore')
 import h5py
+import sys
 import argparse
 
 
 def print_node(node, indent: int =4) -> str:
-    """
+    """Print string representation of a node of a tree
 
     Args:
         node: The node to print
         indent: How many spaces per indent level
 
     Returns:
-        None
+        string representation of this path
 
     Examples:
         >>> print_node("a/b/c", indent=4)
@@ -30,7 +31,7 @@ def print_node(node, indent: int =4) -> str:
     *first, last = str(node).split("/")
     print(" " * indent * len(first) + last)
 
-def print_h5(h5filename, indent = 4) -> object:
+def print_h5(h5filename, *, indent = 4):
     """For a given HDF5file, print its contents"""
     with h5py.File(h5filename) as h5file:
         h5file.visit(lambda node: print_node(node, indent=indent))
@@ -38,12 +39,12 @@ def print_h5(h5filename, indent = 4) -> object:
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("h5file", help="HDF5 file fto display")
+    parser.add_argument("h5file", help="HDF5 file to display")
 
     parser.add_argument("-i", "--indent", help="Indentation (default 4)",
                         type=int, default=4)
     args = parser.parse_args()
-    print(args.h5file)
+    print_h5(args.h5file, indent=args.indent)
 
     #print_h5("/home/josan/Annecy2018/School2018/codingstyle/lofar-cal.h5")
 
